63af64bca3b4e0aadc5b043b69e96d00a0233f5f
TomJourney <pacosongreen@gmail.com>
2025-06-07 15:20:56 +0800

    layer normalization study note

--- src/chapter04/test0402_p90_layer_norm.py
+++ src/chapter04/test0402_p90_layer_norm.py
@@ -8,27 +8,42 @@ batch_example = torch.randn(2, 5)
 layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
 layer_out = layer(batch_example)
 print("层处理（输出）结果-layer_out = ", layer_out)
+# 层处理（输出）结果-layer_out =  tensor([[0.2260, 0.3470, 0.0000, 0.2216, 0.0000, 0.0000],
+#         [0.2133, 0.2394, 0.0000, 0.5198, 0.3297, 0.0000]],
+#        grad_fn=<ReluBackward0>)
 
 # 步骤2： 上述编写的神经网络层包括一个线性层和一个非线性激活函数ReLU（修正线性单元）， ReLU是神经网络中的一种标准激活函数
 print("\n=== 层归一化前，检查层处理结果的均值与方差")
-layer_out_mean = layer_out.layer_norm_result_mean(dim=-1, keepdim=True)
+layer_out_mean = layer_out.mean(dim=-1, keepdim=True)
 layer_out_variance = layer_out.var(dim=-1, keepdim=True)
 print("均值mean = ", layer_out_mean, "\n方差variance = ", layer_out_variance)
+# 均值mean =  tensor([[0.1324],
+#         [0.2170]], grad_fn=<MeanBackward1>)
+# 方差variance =  tensor([[0.0231],
+#         [0.0398]], grad_fn=<VarBackward0>)
 
 # 步骤3：层归一化操作：具体方法是减少均值，并将结果除以方差的平方根（即标准差）
 print("\n===步骤3：层归一化操作：具体方法是减少均值，并将结果除以方差的平方根（即标准差）")
 layer_out_norm = (layer_out - layer_out_mean) / torch.sqrt(layer_out_variance)
-layer_out_norm_mean = layer_out_norm.layer_norm_result_mean(dim=-1, keepdim=True)
+layer_out_norm_mean = layer_out_norm.mean(dim=-1, keepdim=True)
 layer_out_norm_variance = layer_out_norm.var(dim=-1, keepdim=True)
 print("层归一化结果 layer_out_norm = ", layer_out_norm)
 print("层归一化结果均值 layer_out_norm_mean = ", layer_out_norm_mean)
 print("层归一化结果方差 layer_out_norm_variance = ", layer_out_norm_variance)
+# 层归一化结果 layer_out_norm =  tensor([[ 0.6159,  1.4126, -0.8719,  0.5872, -0.8719, -0.8719],
+#         [-0.0189,  0.1121, -1.0876,  1.5173,  0.5647, -1.0876]],
+#        grad_fn=<DivBackward0>)
+# 层归一化结果均值 layer_out_norm_mean =  tensor([[9.9341e-09],
+#         [0.0000e+00]], grad_fn=<MeanBackward1>)
+# 层归一化结果方差 layer_out_norm_variance =  tensor([[1.0000],
+#         [1.0000]], grad_fn=<VarBackward0>)
 
 # 为提高可读性， 通过将sci_mode设置为False来关闭科学计数法，从而在打印张量值时避免使用科学计数法
 print("\n\n=== 通过将sci_mode设置为False来关闭科学计数法")
 torch.set_printoptions(sci_mode=False)
 print("层归一化结果均值 layer_out_norm_mean = ", layer_out_norm_mean)
 print("层归一化结果方差 layer_out_norm_variance = ", layer_out_norm_variance)
-
-
-
+# 层归一化结果均值 layer_out_norm_mean =  tensor([[    0.0000],
+#         [    0.0000]], grad_fn=<MeanBackward1>)
+# 层归一化结果方差 layer_out_norm_variance =  tensor([[1.0000],
+#         [1.0000]], grad_fn=<VarBackward0>)
