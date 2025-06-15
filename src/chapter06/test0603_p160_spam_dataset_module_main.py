a98f66a76ceefa16d25e983dcc23d5cac5ab77c8
TomJourney <pacosongreen@gmail.com>
2025-06-15 17:16:45 +0800

    Diy Spam Dataset

---
+++ src/chapter06/test0603_p160_spam_dataset_module_main.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import tiktoken
+
+from src.chapter06.test0603_p160_spam_dataset_module import DiySpamDataset
+
+# 获取tiktoken中的gpt2分词器
+gpt2_tokenizer = tiktoken.get_encoding("gpt2")
+# 把<|endoftext|>作为填充词元，词元<|endoftext|>的词元id等于50256
+print(gpt2_tokenizer.encode("<|endoftext|>", allowed_special={"<|endoftext|>"}))
+# [50256]
+
+# 创建训练集
+train_dataset = DiySpamDataset(
+    csv_file=Path("dataset") / "train.csv",
+    max_length=None,
+    tokenizer=gpt2_tokenizer
+)
+# 最长序列长度
+print("train_dataset.max_length = ", train_dataset.max_length)
+# train_dataset.max_length =  120
+
+# 创建验证集
+validate_dataset = DiySpamDataset(
+    csv_file=Path("dataset") / "validation.csv",
+    max_length=train_dataset.max_length,
+    tokenizer=gpt2_tokenizer
+)
+
+# 创建测试集
+test_dataset = DiySpamDataset(
+    csv_file=Path("dataset") / "test.csv",
+    max_length=train_dataset.max_length,
+    tokenizer=gpt2_tokenizer
+)
+
+
+
